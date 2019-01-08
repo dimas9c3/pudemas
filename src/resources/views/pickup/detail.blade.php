2a2f5501bf8b60f2f890e0f01196b0291677ff80
Dimas hermawan <dimas9c3@gmail.com>
2019-01-08 15:56:49 +0700

    function locate kurir local unfinished

--- src/resources/views/pickup/detail.blade.php
+++ src/resources/views/pickup/detail.blade.php
@@ -160,49 +160,55 @@
 		document.getElementById("dropdown-pickup").classList.add('show');
 
 		@if($pickup[0]->status != '3')
-
+        
 		var map = L.map('map-courier-detail');
 
-		L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZGltYXM5YzMiLCJhIjoiY2pxbHdvaHM2MDYyNzQycDd4ZGh4dHBmdiJ9.FzOWqlb9-ejzNFoKuqY1RA', {
-			maxZoom: 18,
-			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-			'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-			id: 'mapbox.streets'
-		}).addTo(map);
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+		maxZoom: 18,
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		id: 'mapbox.streets'
+	}).addTo(map);
 
-		// placeholders for the L.marker and L.circle representing user's current position and accuracy    
-		var current_position, current_accuracy;
+	 // placeholders for the L.marker and L.circle representing user's current position and accuracy    
+    var current_position, current_accuracy;
 
-		function onLocationFound(e) {
-		  // if position defined, then remove the existing position marker and accuracy circle from the map
-		  if (current_position) {
-		  	map.removeLayer(current_position);
-		  	map.removeLayer(current_accuracy);
-		  }
+    function onLocationFound(e) {
+      // if position defined, then remove the existing position marker and accuracy circle from the map
+      if (current_position) {
+          map.removeLayer(current_position);
+          map.removeLayer(current_accuracy);
+          console.log(e.latlng.lat + ", " + e.latlng.lng);
+      }
 
-		  var radius = e.accuracy / 2;
+      var radius = e.accuracy / 2;
 
-		  current_position = L.marker(e.latlng).addTo(map)
-		  .bindPopup("You are within " + radius + " meters from this point").openPopup();
+      current_position = L.marker(e.latlng).addTo(map)
+        .bindPopup("You are within " + radius + " meters from this point").openPopup();
 
-		  current_accuracy = L.circle(e.latlng, radius).addTo(map);
-		}
+      current_accuracy = L.circle(e.latlng, radius).addTo(map);
+    }
 
-		function onLocationError(e) {
-			alert(e.message);
-		}
+    function onLocationError(e) {
+      alert(e.message);
+      console.log(e.message);
+    }
 
-		map.on('locationfound', onLocationFound);
-		map.on('locationerror', onLocationError);
+    map.on('locationfound', onLocationFound);
+    map.on('locationerror', onLocationError);
 
-		// wrap map.locate in a function    
-		function locate() {
-			map.locate({setView: true, maxZoom: 16});
-		}
+    // wrap map.locate in a function    
+    function locate() {
+      map.locate({setView: true, maxZoom: 18, watch: false, enableHighAccuracy: false , maximumAge:120000, timeout: 240000});
+    }
 
-		// call locate every 3 seconds... forever
-		setInterval(locate, 5000);
+    // call locate every .. seconds... forever
+    setInterval(locate, 60000);
+		
+		map.on('click', function(e) {
+            alert("Lat, Lon : " + e.latlng.lat + ", " + e.latlng.lng)
+        });
 
 		@elseif($pickup[0]->status == 0)
 		console.log('Job Selesai')
